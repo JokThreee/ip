e3209418695c766cdeeff328a1777503fa54e82c
Tan Wei Jie <e1384470@u.nus.edu>
2026-08-17 22:45:20 +0800

    Add Level-0 chatbot greeting

---
+++ src/main/java/ChimpanziniBananini.java
@@ -0,0 +1,9 @@
+public class ChimpanziniBananini {
+    public static void main(String[] args) {
+        System.out.println("BOMBARDIRO CROCODILO!!!");
+        System.out.println("ChimpanziniBananini has entered the chat.");
+        System.out.println("What can I cook for you, sigma?");
+        System.out.println();
+        System.out.println("Tung tung tung sahur... I'm outta here 💀");
+    }
+}
